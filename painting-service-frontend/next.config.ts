d2a68a58a1fd193a0d9aaee40445df6d8160c345
mehdimatloub <104140239+mehdimatloub@users.noreply.github.com>
2025-01-30 19:54:08 +0100

    Update next.config.ts

--- painting-service-frontend/next.config.ts
+++ painting-service-frontend/next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Si tu as des routes dynamiques, active `trailingSlash`
+  trailingSlash: true,
 };
 
 export default nextConfig;
