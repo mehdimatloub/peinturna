ecd8e732f1fa36fb427d3254584c2886e97f6539
mehdimatloub <104140239+mehdimatloub@users.noreply.github.com>
2025-01-30 21:30:50 +0100

    Delete painting-service-frontend/tailwind.config.js

--- painting-service-frontend/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-export const content = [
-  "./app/**/*.{js,ts,jsx,tsx}", // Inclut les fichiers dans "app/"
-  "./components/**/*.{js,ts,jsx,tsx}", // Inclut les composants
-];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
