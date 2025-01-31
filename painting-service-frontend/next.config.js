b967e16f4e7bdce7bb6e561caae924d00fc1d6db
mehdimatloub <104140239+mehdimatloub@users.noreply.github.com>
2025-01-31 03:45:13 +0100

    Update next.config.js

--- painting-service-frontend/next.config.js
+++ painting-service-frontend/next.config.js
@@ -1,12 +1,15 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
   },
-  plugins: [],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
+
+module.exports = nextConfig;
